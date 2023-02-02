b3100c38457bb9896cc70cee7461fd6de3648c68
Lenovo <Lenovo@LAPTOP-P9CTSAP4>
2023-02-02 21:28:10 +0800

    tugas

---
+++ src/Quizwhil2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Lenovo
+ */
+public class Quizwhil2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i=1; i<=200; i+=2){
+            if (i%3 !=0){
+        }
+            System.out.print(i+"");
+        }
+        System.out.println("");
+    }
+    
+}
